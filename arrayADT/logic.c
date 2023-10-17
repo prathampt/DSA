9fb45bfe8e705ef9cfc996b33d54261ccb1c1c82
prathampt <prathamt12345@gmail.com>
2023-10-17 13:05:22 +0530

    modified previous functions and written append, insert functions

--- arrayADT/logic.c
+++ arrayADT/logic.c
@@ -12,31 +12,58 @@ void init(Array *arr, int size){
 }
 
 void fill(Array * arr){
-    for (int i = arr->length; i < arr->size; i++){
+    int length = arr->length;
+
+    for (int i = length; i < arr->size; i++){
         arr->A[i] = rand() % 100 + 1;
     }
 
+    arr->length = arr->size;
     return;    
 }
 
 void display(Array arr){
+    int length = arr.length;
+
     printf("Array: [");
 
-    for (int i = 0; i < arr.length; i++)
+    for (int i = 0; i < length; i++)
     {
         printf("%d, ", arr.A[i]);
     }
 
-    printf("\b\b ]");
+    printf("\b\b ]\n");
     return;
     
 }
 
-void append(Array *arr, int element);
-void insert(Array *arr, int element, int position);
-void removeElement(Array *arr, int element);
-void removeAtIndex(Array *arr, int index);
+void append(Array *arr, int element){
+    if (arr->length == arr->size) return;
+
+    arr->A[arr->length++] = element;
+    return;    
+}
+
+void insert(Array *arr, int element, int position){
+    int length = arr->length, temp;
+
+    if (length == arr->size || position < 0 || position >= length) return;
+
+    for (int i = length + 1; i > position; i--)
+    {
+        arr->A[i] = arr->A[i-1];
+    }
+
+    arr->A[position] = element;
+
+    arr->length++;
+    return;
+}
+
+int removeElement(Array *arr, int element);
+int removeAtIndex(Array *arr, int index);
 void reverse(Array *arr);
 int min(Array arr);
 int max(Array arr);
 Array * merge(Array arr1, Array arr2);
+void sort(Array *arr);
